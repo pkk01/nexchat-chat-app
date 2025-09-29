09ea1704848828351298c0798d3a2aaa2efd1abf
Pratham Kumar <prk61dlw@gmail.com>
2025-09-30 00:50:31 +0530

    feat: initialize frontend for realtime chat application with Vite and React
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite logo SVG in public directory.
    - Created initial App component with Router and Navbar.
    - Added React logo SVG in assets directory.
    - Set up main entry point in main.jsx with StrictMode.
    - Created placeholder components for Navbar, ProtectedRoute, and various pages (Chat, Login, MainPage, PrivateChat, Signup).
    - Added empty CSS files for styling.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ Frontend/realtimechat-fe/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+
+      <div className="App">
+
+        <Navbar/>
+
+        
+      </div>
+    </Router>
+  )
+}
